bbd1d80b28d123d9df032ab66d0bb46250fede4e
salozher <salozher@users.noreply.github.com>
2019-05-20 13:28:18 +0200

    initial commit

---
+++ src/DataSource/drinkButtonData.js
@@ -0,0 +1,65 @@
+const buttonData = [
+    {
+        id: "1",
+        text: "Americano",
+        callName: "makeAmericano",
+        component1Available: true,
+        component2Available: true,
+        component3Available: true,
+        component4Available: true,
+        grayedOut: false,
+        isSelected: false
+    },
+    {
+        id: "2",
+        text: "Cappuccino",
+        component1Available: true,
+        component2Available: true,
+        component3Available: true,
+        component4Available: true,
+        grayedOut: false,
+        isSelected: false
+    },
+    {
+        id: "3",
+        text: "Wiener Melange",
+        component1Available: true,
+        component2Available: true,
+        component3Available: true,
+        component4Available: true,
+        grayedOut: false,
+        isSelected: false
+    },
+    {
+        id: "4",
+        text: "Choco",
+        component1Available: true,
+        component2Available: true,
+        component3Available: true,
+        component4Available: true,
+        grayedOut: false,
+        isSelected: false
+    },
+    {
+        id: "5",
+        text: "Black Tea",
+        component1Available: true,
+        component2Available: true,
+        component3Available: true,
+        component4Available: true,
+        grayedOut: false,
+        isSelected: false
+    },
+    {
+        id: "6",
+        text: "Earl Grey",
+        component1Available: true,
+        component2Available: true,
+        component3Available: true,
+        component4Available: true,
+        grayedOut: false,
+        isSelected: false
+    }
+]
+
+export default buttonData
